d00d650867540d35e33f8493640e10b702c6a505
Roman Sulaymanov <92111647+DriverOnLips@users.noreply.github.com>
2024-11-30 21:59:10 +0300

    feat: обновил список чатов

--- src/app/chats/_chatsList/page.tsx
+++ src/app/chats/_chatsList/page.tsx
@@ -6,16 +6,16 @@ import { cn } from '@/lib/utils';
 const ChatsList = () => {
 	const chats = [
 		{ id: 1, link: '/chats/1' },
-		{ id: 2, link: '/chats/2' },
-		{ id: 3, link: '/chats/3' },
-		{ id: 4, link: '/chats/4' },
-		{ id: 5, link: '/chats/5' },
-		{ id: 6, link: '/chats/6' },
-		{ id: 7, link: '/chats/7' },
-		{ id: 8, link: '/chats/8' },
-		{ id: 9, link: '/chats/9' },
-		{ id: 10, link: '/chats/10' },
-		{ id: 11, link: '/chats/11' },
+		// { id: 2, link: '/chats/2' },
+		// { id: 3, link: '/chats/3' },
+		// { id: 4, link: '/chats/4' },
+		// { id: 5, link: '/chats/5' },
+		// { id: 6, link: '/chats/6' },
+		// { id: 7, link: '/chats/7' },
+		// { id: 8, link: '/chats/8' },
+		// { id: 9, link: '/chats/9' },
+		// { id: 10, link: '/chats/10' },
+		// { id: 11, link: '/chats/11' },
 	];
 
 	return (
